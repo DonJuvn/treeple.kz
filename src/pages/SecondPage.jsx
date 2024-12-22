e71b98ce6d102b30bd168f10d4bf72560bb85e4a
DonJuan <armvnov1ch@gmail.com>
2024-12-22 17:52:06 +0500

    second page responsive

--- src/pages/SecondPage.jsx
+++ src/pages/SecondPage.jsx
@@ -1,4 +1,5 @@
-import React from "react";const SecondPage = () => {
+import React from "react";
+const SecondPage = () => {
    return (
       <div id="second-page">
          <div className="list">
@@ -28,26 +29,37 @@ import React from "react";const SecondPage = () => {
             </div>
             <div className="mark">
                <img src="img/mark.png" alt="" />
-               <p>
-                  <b>Almaty</b> is a vibrant city with a unique blend of
-                  culture, nature, and modernity. Let's plan an exciting
-                  itinerary for your trip! <br />
-                  <br />
-                  Day 1: Arrival in Almaty Upon your arrival
-                  at Almaty International Airport, make your way to your
-                  accommodation at <b>Ritz-Carlton Almaty﻿</b>. After settling
-                  in, take a leisurely stroll around the city center to get
-                  acquainted with the local atmosphere. <br />
-                  <br />
-                  Day 2: Explore Almaty's Cultural Heritage Start your day by
-                  visiting <b>Central State Museum of Kazakhstan﻿</b> to learn
-                  about the country's rich history. Then, head to 
-                  <b>Panfilov Park﻿</b>, where you can admire the stunning 
-                  <b>Zenkov Cathedral﻿</b>. For lunch, enjoy traditional Kazakh
-                  cuisine at <b>Gakku Restaurant﻿</b>.
-               </p>
+               <div className="text">
+                  <p>
+                     <b>Almaty</b> is a vibrant city with a unique blend of
+                     culture, nature, and modernity. Let's plan an exciting
+                     itinerary for your trip! <br />
+                     <br />
+                     Day 1: Arrival in Almaty Upon your arrival
+                     at Almaty International Airport, make your way to your
+                     accommodation at <b>Ritz-Carlton Almaty﻿</b>. After
+                     settling in, take a leisurely stroll around the city center
+                     to get acquainted with the local atmosphere. <br />
+                     <br />
+                  </p>
+                  <p id="none">
+                     {" "}
+                     Day 2: Explore Almaty's Cultural Heritage Start your day by
+                     visiting <b>Central State Museum of Kazakhstan﻿</b> to
+                     learn about the country's rich history. Then, head to 
+                     <b>Panfilov Park﻿</b>, where you can admire the stunning 
+                     <b>Zenkov Cathedral﻿</b>. For lunch, enjoy traditional
+                     Kazakh cuisine at <b>Gakku Restaurant﻿</b>.
+                  </p>
+               </div>
+            </div>
+            <div className="ritz">
+               <img src="img/ritz-carlton.png" alt="" />
+               <h1>The Ritz-Carlton Almaty</h1>
+               <p>Almaty</p>
             </div>
             <div className="chat">
+               <button id="hint">What can i ask Treeple.kz? </button>
                <div className="input">
                   <input
                      type="text"
@@ -65,7 +77,7 @@ import React from "react";const SecondPage = () => {
             </div>
          </div>
          <div className="right">
-            <img id='increase' src="img/increase.png" alt="" />
+            <img id="increase" src="img/increase.png" alt="" />
             <div className="name">
                <h1>The Ritz-Carlton Almaty</h1>
                <div className="reviews">
@@ -73,9 +85,12 @@ import React from "react";const SecondPage = () => {
                   <p id="black">4.69</p>
                   <p>3.2k reviews Almaty</p>
                </div>
-               <p><img src="img/bed.png" alt="" />Hotel</p>
+               <p>
+                  <img src="img/bed.png" alt="" />
+                  Hotel
+               </p>
             </div>
-            <img id='collage' src="img/collage.png" alt="" />
+            <img id="collage" src="img/collage.png" alt="" />
             <div className="list2">
                <p id="active">Overview</p>
                <p>Rooms</p>
@@ -86,7 +101,13 @@ import React from "react";const SecondPage = () => {
             </div>
             <div className="bottom">
                <div className="left">
-                  <p>The Ritz-Carlton Almaty is a luxury hotel located within the Esentai Tower, one of the tallest buildings in Kazakhstan. This hotel offers panoramic views of the city and the Trans-Ili Alatau mountains. It features elegantly designed rooms and suites...</p>
+                  <p>
+                     The Ritz-Carlton Almaty is a luxury hotel located within
+                     the Esentai Tower, one of the tallest buildings in
+                     Kazakhstan. This hotel offers panoramic views of the city
+                     and the Trans-Ili Alatau mountains. It features elegantly
+                     designed rooms and suites...
+                  </p>
                   <a href="#">Read more</a>
                </div>
                <div className="right">
